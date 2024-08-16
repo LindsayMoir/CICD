3225f5b6ca334aa50a7f2a165814259738953d55
LindsayMoir <tragoes@gmail.com>
2024-08-16 08:04:13 -0700

    Hello_World.py removal of line spacing

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,8 @@
-# Hello world test file
-
-
+# Hello world test file for pep8 testing
 def hello_world(test_number):
-
     message = "Hello, World! I want to see if pep8 is working."
     length = len(message)
     print("The length of the message is: ", length)
-
     return test_number
 
 if __name__ == "__main__":
