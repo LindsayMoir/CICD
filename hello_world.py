c9eaa610a00d0647af79e68513421481c144ac20
LindsayMoir <tragoes@gmail.com>
2024-08-16 08:00:39 -0700

    Hello_World.py function call test

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,14 @@
 # Hello world test file
 
-def hello_world():
-    print("Hello, World! I want to see if pep8 is working. The test is to make this line way over 80 characters. I am pretty sure it is well over that now.")
+
+def hello_world(test_number):
+
+    message = "Hello, World! I want to see if pep8 is working."
+    length = len(message)
+    print("The length of the message is: ", length)
+
+    return test_number
 
 if __name__ == "__main__":
-    hello_world()
-    
+    result = hello_world(test_number=1)        
+    print(result)
