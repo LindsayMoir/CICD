3e525b27060eb9ffadd77b5fc727fe89bdd22bf6
LindsayMoir <tragoes@gmail.com>
2024-08-16 08:15:53 -0700

    Added format_test.py

---
+++ format_test.py
@@ -0,0 +1,10 @@
+# Badly formatted example of a Python script
+
+import pandas as pd
+def process_data():
+ df=pd.DataFrame({'A':[1,2,3,4,5],'B':[10,20,30,40,50],'C':['foo','bar','baz','qux','quux'],'D':[1000,2000,3000,4000,5000]})
+ df['E']=df.apply(lambda row:row['A']*row['B']*row['D']/row['A'],axis=1)
+ filtered_df=df[df['E']>1].sort_values(by='E',ascending=False)
+ print("The processed DataFrame after applying complex operations is:\n",filtered_df)
+if __name__=="__main__":
+ process_data()
--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,14 @@
-# Hello world test file for pep8 testing
+# Hello world test file for autopep8 testing
+
 def hello_world(test_number):
+
     message = "Hello, World! I want to see if pep8 is working."
     length = len(message)
     print("The length of the message is: ", length)
+
     return test_number
 
 if __name__ == "__main__":
+
     result = hello_world(test_number=1)        
-    print(result)
+    print('test_numer is:', result)
