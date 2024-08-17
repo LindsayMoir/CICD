bfb212b3d8517a95684e0b533d04cd97cf055f6b
LindsayMoir <tragoes@gmail.com>
2024-08-17 10:07:29 -0700

    Updated conftest to remove conflict

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,8 @@ import pytest
 
 # Add the parent directory (CICD) to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from iris import load_data  # Now this should work
+# Now we can import the module we want to test
+from iris import load_data
 
 @pytest.fixture
 def iris_data():
