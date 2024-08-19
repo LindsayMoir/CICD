24ec0d8d20aaec66a7fad2b36c5057de8d8cde2e
LindsayMoir <tragoes@gmail.com>
2024-08-18 20:04:21 -0700

    Made conftest and the yml file be dynamic

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
+import os
 import sys
 import pytest
 
-# Add the directory where 'iris.py' is located to sys.path
-sys.path.insert(0, '/mnt/d/GitHub/CICD')
+# Add the parent directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..')))
 
 # Import the module we want to test
 from iris import load_data
